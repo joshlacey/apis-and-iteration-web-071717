a89972c0f93ab25e8cebf957673c910cd9565974
joshlacey <williamjoshualacey@gmail.com>
2017-07-18 16:13:22 -0400

    split up get_character_movies_from_api method. created get_characters method. added conditional to show_character_movies

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -2,20 +2,30 @@ require 'rest-client'
 require 'json'
 require 'pry'
 
+#looks up the data string and converts it into a hash from any url
 def look_up(url)
   all = RestClient.get(url)
  hash = JSON.parse(all)
+end 
+
+#
+def get_film_urls(character)
+  character_hash = look_up('http://www.swapi.co/api/people/')
+  film_urls = character_hash["results"].find do |element|
+    element["name"].downcase == character
+  end["films"]
+end
+
+def get_characters
+  character_hash = look_up('http://www.swapi.co/api/people/')
+  character_hash["results"].collect do |element|
+    element["name"]
+  end
 end
 
 def get_character_movies_from_api(character)
-  character_hash = look_up('http://www.swapi.co/api/people/')
-
-  film_urls = character_hash["results"].find do |element|
-    element["name"] == character
-  end["films"]
-
+  film_urls = get_film_urls(character)
   film_urls.collect do |movie_url|
-    binding.pry
     look_up(movie_url)
   end
 end
@@ -28,7 +38,12 @@ def parse_character_movies(films_hash)
 end
 
 def show_character_movies(character)
-  films_hash = get_character_movies_from_api(character)
-  parse_character_movies(films_hash)
+  if get_characters.include? character
+    films_hash = get_character_movies_from_api(character)
+    puts parse_character_movies(films_hash)
+  else
+    puts "Have you even seen the movies?"
+  end
 end
 
+"k"
